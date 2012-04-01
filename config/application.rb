1320650b4f30b623f9eb5d0fc8a61801ef1ef683
John Walsh <psu22ucf@yahoo.com>
2012-04-01 14:52:35 -0400

    User signup complete

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
+
+require File.expand_path('../boot', __FILE__)
+
 require 'rails/all'
+
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
@@ -37,6 +41,6 @@ module SampleApp
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :secret_code]
   end
 end
