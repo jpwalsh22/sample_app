4221f71dd62919c550fd07b92ad3613da780348f
John Walsh <psu22ucf@yahoo.com>
2012-04-19 11:52:46 -0400

    Done with sign in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120416003310) do
+ActiveRecord::Schema.define(:version => 20120419120147) do
 
   create_table "sessions", :force => true do |t|
     t.string   "session_id", :null => false
@@ -29,9 +29,6 @@ ActiveRecord::Schema.define(:version => 20120416003310) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "encrypted_password"
-    t.string   "salt"
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
