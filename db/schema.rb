f1212672f26ce63c21dbf3ee594b77279d302d56
John Walsh <psu22ucf@yahoo.com>
2012-04-22 06:23:04 -0400

    Finish user edit, update, index and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120419120147) do
+ActiveRecord::Schema.define(:version => 20120419210704) do
 
   create_table "sessions", :force => true do |t|
     t.string   "session_id", :null => false
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(:version => 20120419120147) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "encrypted_password"
+    t.string   "salt"
+    t.boolean  "admin",              :default => false
   end
 
 end
